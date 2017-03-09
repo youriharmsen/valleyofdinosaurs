eedfcb053475cba211ab9ae80b805a80e29374e6
youriharmsen <y.j.g.harmsen@gmail.com>
2017-03-09 15:33:55 +0100

    added new dino function

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -2,4 +2,25 @@ class DinosaursController < ApplicationController
   def index
     @dinosaurs = Dinosaur.all
   end
+
+  def show
+    @dinosaur = Dinosaur.find(params[:id])
+  end
+
+  def new
+    @dinosaur = Dinosaur.new
+  end
+
+  def create
+    dinosaur_params = params.require(:dinosaur).permit(:name, :age, :image_url)
+
+    @dinosaur = Dinosaur.new(dinosaur_params)
+
+       if @dinosaur.save
+          redirect_to @dinosaur
+       else
+          render 'new'
+       end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root "dinosaurs#index"
 
   get "dinosaurs" => "dinosaurs#index"
+  get "dinosaurs/new" => "dinosaurs#new", as: :new_dinosaur
+  get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
+
+  post "dinosaurs" => "dinosaurs#create"
 end
