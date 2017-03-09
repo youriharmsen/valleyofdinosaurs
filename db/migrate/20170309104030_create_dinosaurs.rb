9d68c634bd662f25d3caa9f9ee21c27a068ddc0a
youriharmsen <y.j.g.harmsen@gmail.com>
2017-03-09 13:33:53 +0100

    Created dinosaur model

---
+++ db/migrate/20170309104030_create_dinosaurs.rb
@@ -0,0 +1,12 @@
+class CreateDinosaurs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :dinosaurs do |t|
+      t.string :name
+      t.string :string
+      t.integer :age
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+dino1 = Dinosaur.create!( { name: "Iguanodon", age: 3, image_url: "https://goo.gl/wB6q9q" } )
+dino2 = Dinosaur.create!( { name: "Mosasaurus", age: 10, image_url: "https://goo.gl/aoZsYt" } )
+dino3 = Dinosaur.create!( { name: "Hungarosaurus", age: 15, image_url: "https://goo.gl/EqRlgP" } )
